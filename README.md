6027387153a7cd894b4d04f7468d55557502dc29
lucky-trill <109553278+lucky-trill@users.noreply.github.com>
2026-03-22 19:16:56 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Blue Planet: Recontact CSB Templates
+
+This is a module that includes actor templates, item templates, and integrated macros for playing Blue Planet: Recontact using [Custom System Builder](https://foundryvtt.com/packages/custom-system-builder).
+
+![PC Sheet Page 1 Example](https://github.com/lucky-trill/bpr-csb/raw/refs/heads/main/assets/pc-sheet-example-1.webp)
+
+![PC Sheet Page 2 Example](https://github.com/lucky-trill/bpr-csb/raw/refs/heads/main/assets/pc-sheet-example-2.webp)
+
+![NPC Sheet Example](https://github.com/lucky-trill/bpr-csb/raw/refs/heads/main/assets/npc-sheet-example.webp)
+
+![Macro Example](https://github.com/lucky-trill/bpr-csb/raw/refs/heads/main/assets/macros-example.webp)
+
+## License
+
+Blue Planet © Biohazard Games. All rights reserved. The software component of this module is distributed under the MIT license.
+
+Fonts present under the fonts directory are distributed under various open-source terms; please see their individual license files for full details.
---
+++ bpr-csb.js
@@ -0,0 +1,62 @@
+Hooks.once("init", () => {
+    CONFIG.fontDefinitions["Astrohead"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/Astrohead.otf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Crayon Libre"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/CrayonLibre.ttf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Grith"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/Grith.otf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["No Planet B"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/NoPlanetB.otf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Kepler-452b"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/Kepler452b-Regular.ttf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Kepler-452b Monospace"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/Kepler452b-Mono.ttf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Reckoner"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/Reckoner.ttf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Reckoner Bold"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/ReckonerBold.ttf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Red Planet"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/LjPRedPlanet.otf"] },
+        ],
+    };
+    CONFIG.fontDefinitions["Red Planet Bold"] = {
+        editor: true,
+        fonts: [
+            { urls: ["/modules/bpr-csb/fonts/LjPRedPlanetBold.otf"] },
+        ],
+    };
+});
